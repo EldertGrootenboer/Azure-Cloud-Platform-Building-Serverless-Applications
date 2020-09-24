4bb349dd88a1b5e884777a7b7329db8ad010f1ff
Eldert Grootenboer <eldert@eldert.net>
2020-09-24 16:54:23 +0200

    Updated for better deployments

--- assets/code/iac/1-deployment.ps1
+++ assets/code/iac/1-deployment.ps1
@@ -1,11 +1,11 @@
 # What we will be doing in this script.
 #   1. Create an app registration used for authentication between Logic Apps and Event Grid
-#   1. Create a resource group
-#   2. Deploy Azure services
+#   2. Create a resource group
+#   3. Deploy Azure services
 
 # Update these according to the environment
 $subscriptionName = "Visual Studio Enterprise"
-$resourceGroupName = "rg-building-serverless-applications-2"
+$resourceGroupName = "rg-building-serverless-applications-5"
 $appRegistrationName = "sp-building-serverless-applications-event-grid"
 $administratorEmail = "me@eldert.net"
 $basePath = "C:\Users\elder\OneDrive\Sessions\Azure-s-Cloud-Platform-Building-Serverless-Applications"
@@ -32,18 +32,19 @@ if(-not $appRegistration)
 }
 
 # Create the resource group and deploy the resources
-New-AzResourceGroup -Name $resourceGroupName -Location 'West Europe' -Tag @{CreationDate=[DateTime]::UtcNow.ToString(); Project="Azure-s-Cloud-Platform-Building-Serverless-Applications"; Purpose="Session"}
-New-AzResourceGroupDeployment -Name "BuildServerlessApps" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\code\iac\azuredeploy.json" -administratorObjectId $administratorObjectId -servicePrincipalPasswordEventGrid $clientSecret -servicePrincipalClientIdEventGrid $appRegistration.ApplicationId
+New-AzResourceGroup -Name $resourceGroupName -Location 'West Europe' -Tag @{CreationDate=[DateTime]::UtcNow.ToString(); Project="Azure’s Cloud Platform - Building Serverless Applications"; Purpose="Session"}
+New-AzResourceGroupDeployment -Name "BuildServerlessApps1" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\code\iac\azuredeploy.1.json" -administratorObjectId $administratorObjectId -servicePrincipalPasswordEventGrid $clientSecret
 
 # Deploy contents of the App Service
 dotnet publish "$basePath\assets\code\pgp-encryptor\Azure-s-Cloud-Platform-Building-Serverless-Applications.csproj" -c Release -o "$basePath\assets\code\pgp-encryptor\publish"
 Compress-Archive -Path "$basePath\assets\code\pgp-encryptor\publish\*" -DestinationPath "$basePath\assets\code\pgp-encryptor\Deployment.zip"
-Publish-AzWebapp -ResourceGroupName $resourceGroupName -Name $appServiceName -ArchivePath "$basePath\assets\code\pgp-encryptor\Deployment.zip"
+$functionApp = Get-AzResource -ResourceGroupName $resourceGroupName -Name func-*
+Publish-AzWebapp -ResourceGroupName $resourceGroupName -Name $functionApp.Name -ArchivePath "$basePath\assets\code\pgp-encryptor\Deployment.zip"
 Remove-Item "$basePath\assets\code\pgp-encryptor\Deployment.zip"
 
-# Deploy contract processing Logic App
-# Can not be done from orchestrator as it needs the Function to first be deployed
-New-AzResourceGroupDeployment -Name "BuildServerlessApps" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\code\iac\logic-app-contract-processing.json"
+# Deploy event grid API and contract  processing Logic App
+# Can not be done from first orchestrator as they needs specific other resources to first be deployed
+New-AzResourceGroupDeployment -Name "BuildServerlessApps2" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\code\iac\azuredeploy.2.json" -servicePrincipalClientIdEventGrid $appRegistration.ApplicationId
 
 # Optional for debugging, loops through each local file individually
 #Get-ChildItem "$basePath\assets\code\iac" -Filter *.json | 
